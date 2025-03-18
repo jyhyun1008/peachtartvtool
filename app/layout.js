924cc5e4937ad191febb4e709092beb4cd7f6d3a
jyhyun1008 <jayhyun1008@gmail.com>
2025-03-19 07:57:00 +0900

    임시로 페이지 개설해 둠

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata = {
@@ -12,12 +12,12 @@ export default function RootLayout({ children }) {
       <body>
         <div style={{width: '100vw', height: '3rem', background: 'var(--background)', position: 'fixed', left: 0, top: 0, zIndex: 999, boxShadow: '3px 3px 5px #00000022',display: 'flex', gap: '2rem', alignItems: 'center', justifyContent: 'space-between'}}>
           <div style={{display: 'flex', gap: '2rem', marginLeft: '2rem'}}>
-            <div><b><span>PeachtArt</span></b></div>
-            <div>라이브</div>
-            <div>영상</div>
+            <div><b><span><Link href="/">PeachtArt</Link></span></b></div>
+            <div><Link href="/live" style={{color: 'var(--foreground)'}}>라이브</Link></div>
+            <div><Link href="/video" style={{color: 'var(--foreground)'}}>영상</Link></div>
           </div>
           <div style={{display: 'flex', gap: '2rem', marginRight: '2rem'}}>
-            <div>마이페이지</div>
+            <div><Link href="/member/1" style={{color: 'var(--foreground)'}}>마이페이지</Link></div>
           </div>
         </div>
         {children}
