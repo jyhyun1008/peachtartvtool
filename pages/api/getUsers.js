cd102c1cc3121488c19bdb596186693b8ef478ac
jyhyun1008 <jayhyun1008@gmail.com>
2025-03-20 16:37:22 +0900

    으..

--- pages/api/getUsers.js
+++ pages/api/getUsers.js
@@ -4,19 +4,19 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
         let result = await getUsers()
 
-        function shuffle(array) {
-            if (array.length > 1) {
+        // function shuffle(array) {
+        //     if (array.length > 1) {
 
-                for (let i = array.length - 1; i > 0; i--) {
-                    // 무작위로 index 값 생성 (0 이상 i 미만)
-                let j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-              }
-            }
-            return array
-        }
+        //         for (let i = array.length - 1; i > 0; i--) {
+        //             // 무작위로 index 값 생성 (0 이상 i 미만)
+        //         let j = Math.floor(Math.random() * (i + 1));
+        //         [array[i], array[j]] = [array[j], array[i]];
+        //       }
+        //     }
+        //     return array
+        // }
 
-        let result2 = shuffle(result)
-        res.status(200).json({rows: result2})
+        // let result2 = shuffle(result)
+        res.status(200).json({rows: result})
     }
 }
