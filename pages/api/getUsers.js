5978cb135fe3645d2708821cf11c96d1a38d8e2a
jyhyun1008 <jayhyun1008@gmail.com>
2025-03-20 16:15:35 +0900

    Update getUsers.js

--- pages/api/getUsers.js
+++ pages/api/getUsers.js
@@ -5,10 +5,13 @@ export default async function handler(req, res) {
         let result = await getUsers()
 
         function shuffle(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                  // 무작위로 index 값 생성 (0 이상 i 미만)
-              let j = Math.floor(Math.random() * (i + 1));
-              [array[i], array[j]] = [array[j], array[i]];
+            if (array.length > 1) {
+
+                for (let i = array.length - 1; i > 0; i--) {
+                    // 무작위로 index 값 생성 (0 이상 i 미만)
+                let j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+              }
             }
             return array
         }
